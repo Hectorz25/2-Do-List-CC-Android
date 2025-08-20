549c5214e1abc78708daae3303a45a2d35c5f81a
Hector Islas <hector2299@gmail.com>
2025-08-19 20:18:37 -0700

    initial commit

---
+++ app/src/main/java/com/examenconcredito/a2_dolistapp/SplashScreenActivity.kt
@@ -0,0 +1,41 @@
+package com.examenconcredito.a2_dolistapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splashscreen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val swDarkMode = findViewById<SwitchMaterial>(R.id.swTheme)
+        swDarkMode.setOnCheckedChangeListener {_, isSelected ->
+            if(isSelected){
+                enableDarkMode()
+            }else{
+                disableDarkMode()
+            }
+        }
+    }
+    private fun enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        delegate.applyDayNight()
+    }
+
+    private fun disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        delegate.applyDayNight()
+    }
+}
